dd7f5dc7f4f94fe164c3aaa780b97f9d66d29fd1
Jennie Long <longjen100@gmail.com>
2024-11-14 12:59:24 -0800

    added navigation bar and routes for other pages.

--- src/app.js
+++ src/app.js
@@ -2,13 +2,33 @@ const express = require("express");
 const app = express()
 const port = 3000;
 
-const path = require('path')
-app.use(express.static(path.join(__dirname, 'public')))
+const path = require("path")
+
+function getPath(url) {
+    let endUrl = "public/" + url;
+    if (url != "/") {
+        endUrl += ".html";
+    }
+    return express.static(path.join(__dirname, endUrl));
+}
+
+app.use("/", getPath("/"))
+app.use("/stats", getPath("stats"))
+app.use("/settings", getPath("settings"))
 
 app.get("/", (req, res) => {
-    res.send(":(")
+    res.send("main page")
 })
 
+app.get("stats", (req, res) => {
+    res.send("stats page")
+})
+
+app.get("settings", (req, res) => {
+    res.send("settings page")
+})
+
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 })
